92251a451e3202d743d77f41e493677137667ef6
Israel Calderón de la Cruz <icalderond@outlook.com>
2016-05-05 10:14:14 -0500

    Primer commit

---
+++ libs/js/index.js
@@ -0,0 +1,14 @@
+// /* <![CDATA[ */
+//   function get_object(id) {
+//    var object = null;
+//    if (document.layers) {
+//     object = document.layers[id];
+//    } else if (document.all) {
+//     object = document.all[id];
+//    } else if (document.getElementById) {
+//     object = document.getElementById(id);
+//    }
+//    return object;
+//   }
+// get_object("barcode").innerHTML=DrawCode39Barcode(get_object("barcode").innerHTML,2);
+// /* ]]> */
